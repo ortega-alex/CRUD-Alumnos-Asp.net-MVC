38fb625036e71e85caa950b214583abcb53fe67e
Alex Ortega <mortegalex27@outlook.es>
2019-02-19 17:30:47 -0600

    crud alumnos mvc

---
+++ CRUD-Alumnos/Controllers/AlumnoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_Alumnos.Models;
+
+namespace CRUD_Alumnos.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            AlumnosContex db = new AlumnosContex();
+            //List<Alumno> lista = db.Alumno.Where(a => a.Edad > 18).ToList();
+
+            return View(db.Alumno.ToList());
+        }
+       
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Agregar(Alumno a)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new AlumnosContex())
+                {
+                    a.FechaRegisro = DateTime.Now;
+
+                    db.Alumno.Add(a);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex) {
+                ModelState.AddModelError("" , "Error al registrar alumno" + ex.Message);
+                return View();
+            }  
+        }
+    }
+}
